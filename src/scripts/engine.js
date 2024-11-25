bcf88ff05d8603f78d17d32c3abd12539163ed32
Soren <danilo-258@hotmail.com>
2024-11-25 15:46:42 -0300

    feat: extract to method

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -92,7 +92,7 @@ async function setCardsField(cardId) {
 
   state.fieldCards.player.style.display = "block";
   state.fieldCards.computer.style.display = "block";
-
+  await hiddenCardDetails();
   state.fieldCards.player.src = cardData[cardId].img;
   state.fieldCards.computer.src = cardData[computerCardId].img;
 
@@ -101,6 +101,12 @@ async function setCardsField(cardId) {
 
   await updateScore();
   await drawButton(duelResults);
+}
+
+async function hiddenCardDetails() {
+  state.cardSprites.avatar.src = "";
+  state.cardSprites.name.innerText = "";
+  state.cardSprites.type.innerText = "";
 }
 
 async function drawButton(text) {
@@ -174,6 +180,9 @@ async function playAudio(status) {
 }
 
 function init(){
+  state.fieldCards.player.style.display = "none";
+  state.fieldCards.computer.style.display = "none"
+
   drawCards(5, playerSides.player1);
   drawCards(5, playerSides.computer);
 }
